e8cad537a7d5962762f8ce767fd24d53a57fe5b5
Julia Fernandes <134019386+JFernandesLO@users.noreply.github.com>
2024-06-16 02:14:42 -0300

    Update createAccount.js

--- Contracts/scripts/createAccount.js
+++ Contracts/scripts/createAccount.js
@@ -3,11 +3,13 @@ require("dotenv").config();
 
 async function main() {
   const Registry = await ethers.getContractFactory("ERC6551Registry");
-  const registry = await Registry.attach(process.env.ERC6551REGISTRY_ADDRESS);
+  const registry = await Registry.attach(
+    "0x3B15D7b6db923b39E4039C002A98703F246fd81c"
+  );
   const salt = 8;
-  const implementation = "0x9bd9C036aF54024Bb03802c236256491dA52Dac1";
-  const tokenAddress = "0x41C03e28443131668545dbC1c0b9A48002d58586";
-  const tokenId = 18;
+  const implementation = "0xD0DDE18478b6956ade1482250Ea0326285026B4B";
+  const tokenAddress = "0x38C3D508C806FE3Be3a0D9A830893B486C31b49d";
+  const tokenId = 28;
   const chainID = 11155111;
   const initData = "0x";
 
@@ -39,3 +41,4 @@ main().catch((error) => {
   console.error(error);
   process.exit(1);
 });
+
